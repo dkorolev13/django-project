ec6a01a371c15ad4b55a494124fa5c90f57c0bb7
Dmitry Korolev <dkorolev13@gmail.com>
2022-06-29 23:07:56 +0300

    added model and form

--- web/app/face/views.py
+++ web/app/face/views.py
@@ -1,11 +1,17 @@
-import imp
-#from django.http import HttpResponse
-
 from django.shortcuts import render
+from . import models
 
 
 def index(request):
     if request.method == 'POST':
         print(request.POST.get('image_url', ''))
-    context = {'data': 'this is face app'}
+
+        # TO DO save to Url
+        url = models.Url.objects.create(
+            image_url=request.POST.get('image_url', ''))
+        url.save()
+
+    urls = models.Url.objects.all()
+
+    context = {'image_urls': urls}
     return render(request, 'face/index.html', context)
